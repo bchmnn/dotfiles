ce25cfe86ff86f69f671e95ab3259571ccac4828
unknown <jacob.bachmann@posteo.de>
2021-03-29 18:06:10 +0200

    Added aliases: vi, vim

--- .config/PowerShell/profile.ps1
+++ .config/PowerShell/profile.ps1
@@ -7,7 +7,7 @@ if ($host.Name -eq 'ConsoleHost') {
         function ls_git {
                 & "$HOME\scoop\apps\git\current\usr\bin\ls" --color=auto --group-directories-first -hF $args
         }
-        Set-Alias -Name ls -Value ls_git -Option AllScope
+        Set-Alias -Name ls -Value ls_git -Option Private
 
         function la {
                 & "$HOME\scoop\apps\git\current\usr\bin\ls" --color=auto --group-directories-first -lAhF $args
@@ -16,6 +16,9 @@ if ($host.Name -eq 'ConsoleHost') {
         function ll {
                 & "$HOME\scoop\apps\git\current\usr\bin\ls" --color=always --group-directories-first -lAhF $args | less -r
         }
+
+        Set-Alias -Name vim -Value nvim -Option Private
+        Set-Alias -Name vi -Value nvim -Option Private
 }
 
 Invoke-Expression (oh-my-posh.exe --init --shell pwsh --config "$(scoop prefix oh-my-posh3)/themes/material.omp.json")
