f2475b310337c6cb1c2a35fe6731283b97e5b6cf
unknown <jacob.bachmann@posteo.de>
2021-03-30 16:43:13 +0200

    {} is not an array in PowerShell, @() is

--- .bootstrap/win.ps1
+++ .bootstrap/win.ps1
@@ -1,4 +1,6 @@
-if (!$isWindows) {
+$isDesktop = ($PSVersionTable).PSEdition -eq "Desktop"
+
+if (!$isDesktop -and !$isWindows) {
         Write-Warning "This script is targeted on Windows machines only."
         Break
 }
@@ -11,17 +13,17 @@ if (!$isAdmin) {
 }
 
 $reg = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout\'
-$name = 'Scancode Map'
-$val = { `
-        0,   0,   0,   0,   `
-        0,   0,   0,   0,   `
-        5,   0,   0,   0,   ` # 5 entries (incl. null entry)
-        56,  0,   91,  224, ` # left  win  -> left  alt
-        91,  224, 56,  0,   ` # left  alt  -> left  win
-        56,  224, 29,  224, ` # right ctrl -> right alt
-        29,  224, 56,  224, ` # right alt  -> right ctrl
-        0,   0,   0,   0    ` # null entry
-}
+$name = 'Scancode Mapi'
+$val = @(
+        0,   0,   0,   0,
+        0,   0,   0,   0,
+        5,   0,   0,   0,     # 5 entries (incl. null entry)
+        56,  0,   91,  224,   # left  win  -> left  alt
+        91,  224, 56,  0,     # left  alt  -> left  win
+        56,  224, 29,  224,   # right ctrl -> right alt
+        29,  224, 56,  224,   # right alt  -> right ctrl
+        0,   0,   0,   0      # null entry
+)
 
 New-ItemProperty -Path $reg -Name $name -PropertyType Binary -Value $val
 Write-Output "Restart for the changes to take effect"
