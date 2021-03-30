e7a5174335e4908877e0a0de4f1abf4091531847
unknown <jacob.bachmann@posteo.de>
2021-03-30 16:23:58 +0200

    Keyboard Layout registry to Bootstrap script

---
+++ .bootstrap/win.ps1
@@ -0,0 +1,28 @@
+if (!$isWindows) {
+        Write-Warning "This script is targeted on Windows machines only."
+        Break
+}
+
+$isAdmin = [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
+
+if (!$isAdmin) {
+        Write-Warning "We need admin rights to run this script."
+        Break
+}
+
+$reg = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout\'
+$name = 'Scancode Map'
+$val = { `
+        0,   0,   0,   0,   `
+        0,   0,   0,   0,   `
+        5,   0,   0,   0,   ` # 5 entries (incl. null entry)
+        56,  0,   91,  224, ` # left  win  -> left  alt
+        91,  224, 56,  0,   ` # left  alt  -> left  win
+        56,  224, 29,  224, ` # right ctrl -> right alt
+        29,  224, 56,  224, ` # right alt  -> right ctrl
+        0,   0,   0,   0    ` # null entry
+}
+
+New-ItemProperty -Path $reg -Name $name -PropertyType Binary -Value $val
+Write-Output "Restart for the changes to take effect"
+
